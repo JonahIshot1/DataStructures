3d2b2544ce664d35f6e01bc2f826ab5d7156b636
Jonah.plishner <Jonah.plishner@GMR-118-2-2>
2026-06-03 17:31:07 -0700

    your killing it bud. just fix teh part that turns it from a bucket lsit to a normal list <3

---
+++ DataStructures/ContingSort/ContingSort/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+
+namespace ContingSort
+{
+    internal class Program
+    {
+        static int[] sort(int[] list)
+        {
+            int max = 0;
+            for(int i =0; i <list.Length;i++)
+            {
+                if (list[i] > max)
+                { max = list[i]; }
+            }
+            int[] outP = new int[max];
+            for(int i =0; i < list.Length;i++)
+            {
+                outP[list[i]]++;
+            }
+            for(int i =0; i<list.Length;i++)
+            {
+                list[i]= outP
+            }
+            return outP;
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
