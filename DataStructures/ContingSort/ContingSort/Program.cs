3e872021cb283355fb41cdf5aae63f645d18c097
jonah.plishner <jonahevan@icloud.com>
2026-06-10 17:30:50 -0700

    Jonah ur lookign samn spanking awsome also you need to make bucket sort work

--- DataStructures/ContingSort/ContingSort/Program.cs
+++ DataStructures/ContingSort/ContingSort/Program.cs
@@ -1,31 +1,107 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+using System.Windows.Markup;
 
 namespace ContingSort
 {
     internal class Program
     {
-        static int[] sort(int[] list)
+        public int[] sort(int[] list)
         {
+            if (list.Length == 0) return null;
             int max = 0;
             for(int i =0; i <list.Length;i++)
             {
                 if (list[i] > max)
                 { max = list[i]; }
             }
-            int[] outP = new int[max];
+            int[] outP = new int[max+1];
             for(int i =0; i < list.Length;i++)
             {
                 outP[list[i]]++;
             }
-            for(int i =0; i<list.Length;i++)
+            int index = 0;
+            for (int i = 0; i < list.Length; i++)
             {
-                list[i]= outP
+                if (outP[index] == 0)
+                {
+                    index++;
+                    i--;
+                    continue;
+                }
+                list[i] = index;
+                outP[index]--;
             }
             return outP;
         }
         static void Main(string[] args)
         {
-            
+
+            Console.Write("how many number do you want in you list:");
+            string word = Console.ReadLine();
+            int length = 0;
+            while (int.TryParse(word, out length)==false)
+            {
+                Console.Write("THAT DOESNT WORK!!!!!!!!!!!!!! GIVE ME A NEW NUM PLS:");
+                word= Console.ReadLine();
+            }
+
+            Console.Write("whats ur min val:");
+            string word2 = Console.ReadLine();
+            int min = 0;
+            while (int.TryParse(word2, out min) == false)
+            {
+                Console.Write("THAT DOESNT WORK!!!!!!!!!!!!!! GIVE ME A NEW min PLS:");
+                word2 = Console.ReadLine();
+            }
+
+            Console.Write("whats ur max val:");
+            string word3 = Console.ReadLine();
+            int max = 0;
+            while (true)
+            {
+                if(int.TryParse(word3, out max) == true)
+                {
+                    if (max > min)
+                    {
+                        break;
+                    }
+                    
+                }
+                Console.Write("THAT DOESNT WORK!!!!!!!!!!!!!! GIVE ME A NEW max PLS:");
+                word3 = Console.ReadLine();
+            }
+            int[] inP = new int[length];
+            Random randy = new Random();
+            Console.Write("vals:");
+            for(int i =0; i < length;i++)
+            {
+                inP[i]= randy.Next(min,max);
+                Console.Write(inP[i]);
+                if (i < length - 1)
+                {
+                    Console.Write(",");
+                }
+
+            }
+            Console.WriteLine(" ");
+            Sorty.County(inP);
+            Console.Write("OutP:");
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write(inP[i]);
+                if (i < length - 1)
+                {
+                    Console.Write(",");
+                }
+
+            }
+
+
+
+
+
         }
     }
 }
---
+++ DataStructures/ContingSort/ContingSort/Sorty.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContingSort
+{
+    public static class Sorty
+    {
+        public static int[] County(int[] list)
+        {
+            if (list.Length == 0) return null;
+            int max = 0;
+            for (int i = 0; i < list.Length; i++)
+            {
+                if (list[i] > max)
+                { max = list[i]; }
+            }
+            int[] outP = new int[max + 1];
+            for (int i = 0; i < list.Length; i++)
+            {
+                outP[list[i]]++;
+            }
+            int index = 0;
+            for (int i = 0; i < list.Length; i++)
+            {
+                if (outP[index] == 0)
+                {
+                    index++;
+                    i--;
+                    continue;
+                }
+                list[i] = index;
+                outP[index]--;
+            }
+
+            return outP;
+        }
+        public static int[] bucketSort(int[] list)
+        {
+            if (list.Length == 0) return null;
+            int bucketCount = (int)Math.Sqrt(list.Length);
+            int max = 0;
+            for (int i = 0; i < list.Length; i++)
+            {
+                if (list[i] > max)
+                { max = list[i]; }
+            }
+            int partVal = 
+
+            return null;
+        }
+    }
+}
