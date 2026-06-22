5b1e5670415110d1befe1374527a3baa753d6795
Jonah.Plishner <Jonahevan@icloud.com>
2026-06-22 16:57:59 -0700

    we donzo looking cute and extra tall td

--- DataStructures/HashMap/HashMap/Map.cs
+++ DataStructures/HashMap/HashMap/Map.cs
@@ -2,11 +2,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 
 namespace HashMap
 {
-    class Map<TKey, TValue> : IDictionary<TKey, TValue>
+    public class Map<TKey, TValue> : IDictionary<TKey, TValue>
     {
         private readonly IEqualityComparer<TKey> keyComparer;
         private readonly LinkedList<KeyValuePair<TKey, TValue>>[] buckets;
@@ -45,17 +46,17 @@ namespace HashMap
 
         public TValue this[TKey key]
         {
-            get 
-            { 
-                throw new NotImplementedException(); 
+            get
+            {
+                throw new NotImplementedException();
             }
-            set 
-            { 
-                throw new NotImplementedException(); 
+            set
+            {
+                throw new NotImplementedException();
             }
-        }               
+        }
 
-        public Map(IEqualityComparer<TKey> comparer, ICollection<TValue> vals  )
+        public Map(IEqualityComparer<TKey> comparer, ICollection<TValue> vals)
         {
             keyComparer = comparer;
             buckets = new LinkedList<KeyValuePair<TKey, TValue>>[1000];
@@ -78,15 +79,15 @@ namespace HashMap
             int code = key.GetHashCode();
             code.Equals(Math.Abs(code));
             code.Equals(code % buckets.Length);
-            LinkedListNode < KeyValuePair < TKey, TValue >> cur = buckets[code].First;
+            LinkedListNode<KeyValuePair<TKey, TValue>> cur = buckets[code].First;
             for (int i = 0; i < buckets[code].Count; i++)
             {
                 if (cur.Value.Key.Equals(key)) throw new Exception();
-                cur=cur.Next;
+                cur = cur.Next;
             }
 
-            KeyValuePair<TKey,TValue> val = new KeyValuePair <TKey,TValue>(key,value);
-            buckets[code].AddLast(val); 
+            KeyValuePair<TKey, TValue> val = new KeyValuePair<TKey, TValue>(key, value);
+            buckets[code].AddLast(val);
         }
         public bool ContainsKey(TKey key)
         {
@@ -117,7 +118,7 @@ namespace HashMap
                 }
                 cur = cur.Next;
             }
-            return false; 
+            return false;
         }
 
         public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
@@ -157,11 +158,11 @@ namespace HashMap
 
         public void Clear()
         {
-            for(int i =0; i < buckets.Length;i++)
+            for (int i = 0; i < buckets.Length; i++)
             {
                 buckets[i].Clear();
             }
-           
+
         }
 
         public bool Contains(KeyValuePair<TKey, TValue> item)
@@ -180,17 +181,48 @@ namespace HashMap
 
         public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
         {
-            throw new NotImplementedException();
+            LinkedListNode<KeyValuePair<TKey, TValue>> cur = buckets[arrayIndex].First;
+            LinkedList<KeyValuePair<TKey, TValue>> buck = new LinkedList<KeyValuePair<TKey, TValue>>();
+            for (int i = 0; i < buckets[arrayIndex].Count; i++)
+            {
+                array[i] = cur.Value;
+                cur = cur.Next;
+            }
         }
 
         public bool Remove(KeyValuePair<TKey, TValue> item)
         {
-            throw new NotImplementedException();
+            int code = item.Key.GetHashCode();
+            code.Equals(Math.Abs(code));
+            code.Equals(code % buckets.Length);
+            LinkedListNode<KeyValuePair<TKey, TValue>> cur = buckets[code].First;
+            for (int i = 0; i < buckets[code].Count; i++)
+            {
+                if (cur.Value.Key.Equals(item.Key))
+                {
+                    buckets[code].Remove(cur);
+                    return true;
+                }
+                cur = cur.Next;
+            }
+            return false;
         }
 
         public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
         {
-            throw new NotImplementedException();
+            for (int l = 0; l < buckets.Length; l++)
+            {
+                LinkedListNode<KeyValuePair<TKey, TValue>> cur = buckets[l].First;
+                for (int i = 0; i < buckets[l].Count; i++)
+                {
+                    yield return cur.Value;
+                    cur = cur.Next;
+                }
+            }
+
+            //yield return curr.Value; // produce one item
+            //curr = curr.Next;  // move onto the next
+
         }
 
         IEnumerator IEnumerable.GetEnumerator()
---
+++ DataStructures/HashMap/HashMapTest/UnitTest1.cs
@@ -0,0 +1,73 @@
+﻿using HashMap;
+using System.IO.MemoryMappedFiles;
+
+namespace HashMapTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestAdd()
+        {
+            Map<int, string> map = new Map<int, string>();
+            map.Add(1, "a");
+            map.Add(2, "b");
+            Assert.True(map.ContainsKey(1));
+            Assert.True(map.ContainsKey(2));
+            Assert.False(map.ContainsKey(3));
+            Map<int, string> map2 = new Map<int, string>();
+            for (int i =0; i<1000;i++)
+            {
+                map2.Add(i,"a");
+            }
+            Assert.True(map2.ContainsKey(100));
+            String Val = "";
+            map.TryGetValue(10, out Val);
+
+        }
+        [Fact]
+        public void TestGetVal()
+        {
+            Map<int, string> map = new Map<int, string>();
+            map.Add(1, "a");
+            map.Add(2, "b");
+            String Val = "";
+            map.TryGetValue(1, out Val);
+            Assert.True(Val == "a");
+            bool condish = map.TryGetValue(3, out Val);
+            Assert.False(condish);
+
+        }
+        [Fact]
+        public void TestRemove()
+
+        {
+            Map<int, string> map = new Map<int, string>();
+            map.Add(1, "a");
+            map.Add(2, "b");
+            Assert.True(map.ContainsKey(1));
+            map.Remove(1);
+            Assert.False(map.ContainsKey(1));
+            Assert.True(map.ContainsKey(2));
+            Assert.False(map.Remove(1));
+            map.Remove(2);
+            for (int i = 0; i < 1000; i++)
+            {
+                map.Add(i, "a");
+            }
+            Assert.True(map.Remove(214));
+
+
+        }
+        [Fact]
+        public void TestClear()
+        {
+            Map<int, string> map = new Map<int, string>();
+            map.Add(1, "a");
+            map.Clear();
+            map.Add(5, "r");
+            map.Add(7, "l");
+            Assert.False(map.ContainsKey(2));
+            Assert.True(map.ContainsKey(5));
+        }
+    }
+}
